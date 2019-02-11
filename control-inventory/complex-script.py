d8771d23c01487181c3bddfae2a01f46bb41aae0
Sofi <40280900+mutesii@users.noreply.github.com>
2019-02-11 09:56:14 -0800

    Update complex-script.py

--- control-inventory/complex-script.py
+++ control-inventory/complex-script.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python
-# -*- coding: utf8 -*-
-#
-# Ansible dynamic inventory script for reading from a Tower SCM project
-# Requires: ansible, ansible-tower-cli
-#
-#    Copyright © 2016 Red Hat, Inc.
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
 
 import os
 import sys
